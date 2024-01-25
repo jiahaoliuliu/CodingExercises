7a74f9614a795ec71fcbe4c0767392e5b77e4031
Jiahao Liu <jiahaoliuliu@gmail.com>
2024-01-25 14:20:00 +0100

    JL Added Valid anagram exercise

---
+++ src/main/kotlin/arrays/NewYearChaos.kt
@@ -0,0 +1,85 @@
+package arrays.hackerrank
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/*
+* https://www.hackerrank.com/challenges/new-year-chaos/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+* It is New Year's Day and people are in line for the Wonderland rollercoaster ride.
+* Each person wears a sticker indicating their initial position in the queue from  to.
+* Any person can bribe the person directly in front of them to swap positions, but they
+* still wear their original sticker. One person can bribe at most two others.
+
+* Determine the minimum number of bribes that took place to get to a given queue order.
+* Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.
+* Example 1
+*   - q = [1, 2, 3, 5, 4, 6, 7, 8]
+*   - If person  bribes person , the queue will look like this:\
+*       q = [1, 2, 3, 5, 4, 6, 7, 8]
+*     Only  bribe is required. Print 1.
+* Example 2
+*   - q = [4, 1, 2, 3]
+*   - Person 4 had to bribe 3 people to get to the current position. Print Too chaotic.
+*/
+class NewYearChaos {
+    /**
+     * Initial
+     */
+
+    companion object {
+        private const val TOO_CHAOTIC = "Too chaotic"
+    }
+
+    fun minimumBribes(q: Array<Int>): String {
+        // Write your code here
+        var movement = 0
+        for ((index, value) in q.withIndex()) {
+            if (value - index > (2 + 1)) {
+                return TOO_CHAOTIC
+            }
+
+            if (value > index) {
+                movement += value - index - 1
+            }
+        }
+
+        return movement.toString()
+    }
+
+    @Test // 51237864
+    fun test1() {
+        // Given
+        val input = arrayOf(5, 1, 2, 3, 7, 8, 4)
+
+        // When
+        val result = minimumBribes(input)
+
+        // Then
+        assertEquals(TOO_CHAOTIC, result)
+    }
+
+    /**
+     * [1, 2, 5, 3, 7, 8, 6, 4]
+     * [1, 2, 3, 4, 5, 6, 7, 8]
+     * [1, 2, 3, 5, 4, 6, 7, 8]
+     * [1, 2, 5, 3, 4, 6, 7, 8]
+     * [1, 2, 5, 3, 4, 7, 6, 8]
+     * [1, 2, 5, 3, 7, 4, 6, 8]
+     * [1, 2, 5, 3, 7, 4, 8, 6]
+     * [1, 2, 5, 3, 7, 8, 4, 6]
+     * [1, 2, 5, 3, 7, 8, 6, 4] !!
+     * Graph
+     *
+     */
+    @Test // 12537864
+    fun test2() {
+        // Given
+        val input = arrayOf(1, 2, 5, 3, 7, 8, 6, 4)
+
+        // When
+        val result = minimumBribes(input)
+
+        // Then
+        assertEquals("7", result)
+    }
+}
---
+++ src/main/kotlin/arrays/ValidAnagram.kt
@@ -0,0 +1,130 @@
+package arrays
+
+import junit.framework.Assert.assertFalse
+import junit.framework.Assert.assertTrue
+import org.junit.Test
+
+/**
+ * https://leetcode.com/problems/valid-anagram/
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+ *  typically using all the original letters exactly once.
+ */
+class ValidAnagram {
+
+    /**
+     * Optimal solution for when the parameter are only lower case
+     */
+    private fun solution1(s: String, t: String): Boolean {
+        // Corner case check
+        if (s.length != t.length) {
+            return false
+        }
+
+        // Create the map of 26 chars, since there are only 26 chars in English
+        val map = IntArray(26)
+
+        // Build the map with the chars
+        s.toCharArray().forEach {
+            map[it - 'a'] ++
+        }
+
+        // Check the chars against the map
+        t.toCharArray().forEach {
+            map[it - 'a'] --
+        }
+
+        // Check the result
+        map.forEach{
+            if (it != 0) return false
+        }
+        return true
+    }
+
+    private fun solution2(s: String, t:String): Boolean {
+        // Corner case
+        if (s.length != t.length) {
+            return false
+        }
+
+        val charHashMap = HashMap<Char, Int>()
+
+        // Build the char
+        s.toCharArray().forEach {
+            charHashMap[it] = charHashMap.getOrDefault(it, 0) + 1
+        }
+
+        // Check the result
+        t.toCharArray().forEach {char ->
+            var value = charHashMap.getOrDefault(char, 0)
+            charHashMap[char] = --value
+        }
+
+        return charHashMap.filter {(_, v) ->
+            v != 0
+        }.isEmpty()
+    }
+
+    @Test
+    fun test1() {
+        // Given
+        val original = "anagram"
+        val check = "nagaram"
+
+        // When
+        val result1 = solution1(original, check)
+        val result2 = solution2(original, check)
+
+        // Then
+        assertTrue(result1)
+        assertTrue(result2)
+    }
+
+    @Test
+    fun test2() {
+        // Given
+        val original = "rat"
+        val check = "cat"
+
+        // When
+        val result1 = solution1(original, check)
+        val result2 = solution2(original, check)
+
+        // Then
+        assertFalse(result1)
+        assertFalse(result2)
+    }
+
+    @Test
+    fun test3() {
+        // Given
+        val original = "CNBC"
+        val check = "BCCN"
+
+        // When
+        // The solution 1 only works with lower case characters
+//        val result1 = solution1(original, check)
+        val result2 = solution2(original, check)
+
+        // Then
+//        assertTrue(result1)
+        assertTrue(result2)
+    }
+
+    @Test
+    fun test4() {
+        // Given
+        val original = "CNaBC"
+        val check = "BCaCN"
+
+        // When
+        // The solution 1 only works with lower case characters
+//        val result1 = solution1(original, check)
+        val result2 = solution2(original, check)
+
+        // Then
+//        assertTrue(result1)
+        assertTrue(result2)
+    }
+}
