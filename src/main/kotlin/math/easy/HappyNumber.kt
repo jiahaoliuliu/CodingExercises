f0ce4243373db17d7ff3be27432281fbac0bf3f1
Jiahao Liu <jiahaoliuliu@gmail.com>
2024-02-14 12:58:45 +0100

    JL Added the happy number

---
+++ src/main/kotlin/math/easy/HappyNumber.kt
@@ -0,0 +1,64 @@
+package math.easy
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+/**
+ * Happy number
+ *
+ * Write an algorithm to determine if a number n is happy.
+ *
+ * A happy number is a number defined by the following process:
+ * Starting with any positive integer, replace the number by the sum of the squares of its digits.
+ * Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle
+ * which does not include 1.
+ * Those numbers for which this process ends in 1 are happy.
+ * Return true if n is a happy number, and false if not.
+ *
+ * Example 1:
+ *  Input: n = 19
+ *  Output: true
+ *  Explanation:
+ *      1^2 + 9^2 = 82
+ *      8^2 + 2^2 = 68
+ *      6^2 + 8^2 = 100
+ *      1^2 + 0^2 + 0^2 = 1
+ *
+ * Example 2:
+ *      Input: n = 2
+ *      Output: false
+ *
+ * Constraints:
+ *  1 <= n <= 2^31 - 1
+ */
+class HappyNumber {
+
+    private fun isHappy(n: Int): Boolean {
+        return true
+    }
+
+    @Test
+    fun test1() {
+        // Given
+        val input = 19
+
+        // When
+        val result = isHappy(input)
+
+        // Then
+        assertTrue(result)
+    }
+
+    @Test
+    fun test2() {
+        // Given
+        val input = 2
+
+        // When
+        val result = isHappy(input)
+
+        // Then
+        assertFalse(result)
+    }
+}
