ad88f59d085985cf5792d0c2b2a2f5593fa74fc0
Jiahao Liu <jiahaoliuliu@gmail.com>
2025-04-16 22:56:45 +0200

    [ArrayAndList][Medium][Next Permutation] Possible solution. Need optimization

--- src/main/kotlin/arrayandlist/medium/NextPermutation.kt
+++ src/main/kotlin/arrayandlist/medium/NextPermutation.kt
@@ -1,10 +1,5 @@
 package arrayandlist.medium
 
-import linkedlist.ListNode
-import linkedlist.toLinkedList
-import linkedlist.toList
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
@@ -63,9 +58,13 @@ abstract class NextPermutation {
         override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> {
             return Stream.of(
                 Arguments.of(intArrayOf(1, 2, 3), intArrayOf(1, 3, 2)),
-//                Arguments.of(intArrayOf(3, 2, 1), intArrayOf(1, 2, 3)),
-//                Arguments.of(intArrayOf(1, 1, 5), intArrayOf(1, 5, 1)),
-//                Arguments.of(intArrayOf(4, 3, 2, 5, 3, 1), intArrayOf(4, 3, 3, 1, 2, 5)),
+                Arguments.of(intArrayOf(3, 2, 1), intArrayOf(1, 2, 3)),
+                Arguments.of(intArrayOf(1, 1, 5), intArrayOf(1, 5, 1)),
+                Arguments.of(intArrayOf(1, 3, 2), intArrayOf(2, 1, 3)),
+                Arguments.of(intArrayOf(4, 3, 2, 5, 3, 1), intArrayOf(4, 3, 3, 1, 2, 5)),
+                Arguments.of(intArrayOf(1, 2), intArrayOf(2, 1)),
+                Arguments.of(intArrayOf(2), intArrayOf(2)),
+                Arguments.of(intArrayOf(2,3,1,3,3), intArrayOf(2, 3, 3, 1, 3)),
             )
         }
     }
@@ -92,49 +91,50 @@ abstract class NextPermutation {
  */
 class NextPermutationImpl: NextPermutation() {
     override fun nextPermutation(nums: IntArray) {
+        // 0. Corner cases
+        if (nums.size == 1) return
+        if (nums.size == 2) {
+            nums.swap(0, 1)
+            return
+        }
+
         // 1. Find the number to be replaced
         val posToBeReplaced = findPosToBeReplaced(nums)
-        println("Pos to be replaced $posToBeReplaced")
-        // Nothing to be replaced
-        if (posToBeReplaced == -1) {
-            return
-        }
 
         // 2. Element to be replaced
-        val posToReplace = findElementToReplace(posToBeReplaced, nums)
-        println("The element to replace is $posToReplace")
-        if (posToReplace == -1) {
-            return
+        if (posToBeReplaced >= 0) {
+            val posToReplace = findElementToReplace(posToBeReplaced, nums)
+            if (posToReplace >= 0) {
+                nums.swap(posToBeReplaced, posToReplace)
+            }
         }
 
-        val tmp = nums[posToBeReplaced]
-        nums[posToBeReplaced] = nums[posToReplace]
-        nums[posToReplace] = tmp
+        // 3. Revert all the number from the right side because those number are bigger
+        nums.reverse(posToBeReplaced + 1)
+    }
 
-        println("After replace the elements ${nums.toList()}")
-
-        // 3. Sort elements on the right of the posToBeReplaced
-        val listToBeSorted = mutableListOf<Int>()
-        for (i in posToBeReplaced + 1 until nums.size) {
-            listToBeSorted.add(nums[i])
+    private fun IntArray.reverse(startPosition: Int) {
+        var left = startPosition
+        var right = this.size - 1
+        while (left < right) {
+            swap(left, right)
+            left++
+            right--
         }
+    }
 
-        listToBeSorted.sort()
-        println("List sorted $listToBeSorted")
-
-        for (i in posToBeReplaced + 1 until nums.size) {
-            println(i)
-            println(i - posToReplace + 1)
-            nums[i] = listToBeSorted[i - posToReplace + 1]
-        }
-        println(nums.toList())
+    private fun IntArray.swap(i: Int, j: Int) {
+        println("Swapping $i and $j")
+        val tmp = get(i)
+        set(i, get(j))
+        set(j, tmp)
     }
 
     private fun findElementToReplace(posToBeReplaced: Int, nums: IntArray): Int {
         val numToBeReplaced = nums[posToBeReplaced]
         var nextBiggestNumber = Pair(-1, Int.MAX_VALUE)
         for (i in posToBeReplaced + 1 until nums.size) {
-            if (nums[i] in (numToBeReplaced + 1) until nextBiggestNumber.second) {
+            if (nums[i] > numToBeReplaced) {
                 nextBiggestNumber = Pair(i, nums[i])
             }
         }
@@ -149,5 +149,59 @@ class NextPermutationImpl: NextPermutation() {
             }
         }
         return -1
+    }
+}
+
+class NextPermutationClaude: NextPermutation() {
+    override fun nextPermutation(nums: IntArray) {
+        val n = nums.size
+
+        // Step 1: Find the first pair from the right where nums[i-1] < nums[i]
+        var i = n - 1
+        while (i > 0 && nums[i-1] >= nums[i]) {
+            i--
+        }
+
+        // If no such pair exists, the array is in descending order
+        // Step 2: Reverse the array to get the smallest permutation
+        if (i == 0) {
+            nums.reverse()
+            return
+        }
+
+        // Step 3: Find the rightmost element greater than nums[i-1]
+        var j = n - 1
+        while (nums[j] <= nums[i-1]) {
+            j--
+        }
+
+        // Step 4: Swap nums[i-1] and nums[j]
+        val temp = nums[i-1]
+        nums[i-1] = nums[j]
+        nums[j] = temp
+
+        // Step 5: Reverse the subarray starting from position i
+        var left = i
+        var right = n - 1
+        while (left < right) {
+            val temp = nums[left]
+            nums[left] = nums[right]
+            nums[right] = temp
+            left++
+            right--
+        }
+    }
+
+    // Helper function to reverse an array in-place
+    fun IntArray.reverse() {
+        var left = 0
+        var right = this.size - 1
+        while (left < right) {
+            val temp = this[left]
+            this[left] = this[right]
+            this[right] = temp
+            left++
+            right--
+        }
     }
 }
