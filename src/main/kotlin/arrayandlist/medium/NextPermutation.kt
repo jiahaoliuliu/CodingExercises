419d9c207844587266f1cf473a5d65ea80e07cbd
Jiahao Liu <jiahaoliuliu@gmail.com>
2025-04-16 21:24:33 +0200

    [ArrayAndList][Medium][Next permutation] Update the algorithm

--- src/main/kotlin/arrayandlist/medium/NextPermutation.kt
+++ src/main/kotlin/arrayandlist/medium/NextPermutation.kt
@@ -63,16 +63,91 @@ abstract class NextPermutation {
         override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> {
             return Stream.of(
                 Arguments.of(intArrayOf(1, 2, 3), intArrayOf(1, 3, 2)),
-                Arguments.of(intArrayOf(3, 2, 1), intArrayOf(1, 2, 3)),
-                Arguments.of(intArrayOf(1, 1, 5), intArrayOf(1, 5, 1)),
+//                Arguments.of(intArrayOf(3, 2, 1), intArrayOf(1, 2, 3)),
+//                Arguments.of(intArrayOf(1, 1, 5), intArrayOf(1, 5, 1)),
+//                Arguments.of(intArrayOf(4, 3, 2, 5, 3, 1), intArrayOf(4, 3, 3, 1, 2, 5)),
             )
         }
     }
 }
 
+/**
+ * Initial thoughts
+ * 1. Find the number to be replaced
+ * 432531
+ * -> 1 cannot be changed
+ * -> 31 cannot be increased
+ * -> 531 cannot be increased
+ * -> 2531 can be increased
+ * 2. find replacement
+ * 2 cannot be replaced by 1 -> it is not increase
+ * 2 can be replaced by 3 -> It is increase and it is the lowest increase possible
+ *
+ * so the replacement is 3 -> 35_1, 2
+ * 3. Find the smallest element on the right
+ * Sort the elements 5, 2, 1 from smallest to the biggest
+ * 3125
+ *
+ * The result is 433125
+ */
 class NextPermutationImpl: NextPermutation() {
     override fun nextPermutation(nums: IntArray) {
-        nums.reverse()
+        // 1. Find the number to be replaced
+        val posToBeReplaced = findPosToBeReplaced(nums)
+        println("Pos to be replaced $posToBeReplaced")
+        // Nothing to be replaced
+        if (posToBeReplaced == -1) {
+            return
+        }
+
+        // 2. Element to be replaced
+        val posToReplace = findElementToReplace(posToBeReplaced, nums)
+        println("The element to replace is $posToReplace")
+        if (posToReplace == -1) {
+            return
+        }
+
+        val tmp = nums[posToBeReplaced]
+        nums[posToBeReplaced] = nums[posToReplace]
+        nums[posToReplace] = tmp
+
+        println("After replace the elements ${nums.toList()}")
+
+        // 3. Sort elements on the right of the posToBeReplaced
+        val listToBeSorted = mutableListOf<Int>()
+        for (i in posToBeReplaced + 1 until nums.size) {
+            listToBeSorted.add(nums[i])
+        }
+
+        listToBeSorted.sort()
+        println("List sorted $listToBeSorted")
+
+        for (i in posToBeReplaced + 1 until nums.size) {
+            println(i)
+            println(i - posToReplace + 1)
+            nums[i] = listToBeSorted[i - posToReplace + 1]
+        }
+        println(nums.toList())
     }
 
+    private fun findElementToReplace(posToBeReplaced: Int, nums: IntArray): Int {
+        val numToBeReplaced = nums[posToBeReplaced]
+        var nextBiggestNumber = Pair(-1, Int.MAX_VALUE)
+        for (i in posToBeReplaced + 1 until nums.size) {
+            if (nums[i] in (numToBeReplaced + 1) until nextBiggestNumber.second) {
+                nextBiggestNumber = Pair(i, nums[i])
+            }
+        }
+
+        return nextBiggestNumber.first
+    }
+
+    private fun findPosToBeReplaced(nums: IntArray): Int {
+        for (i in (0 until nums.size - 1).reversed()) {
+            if (nums[i] < nums[i+1]) {
+                return i
+            }
+        }
+        return -1
+    }
 }
